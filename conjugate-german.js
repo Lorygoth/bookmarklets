d4f08fa83ef143f6b0f2866987c8cd018b5d7222
Yury <bad.wolf.gallifrey@gmail.com>
2021-06-05 10:41:43 +0200

    add search for german words on conjugator.reverso.net

---
+++ conjugate-german.js
@@ -0,0 +1,4 @@
+javascript: (function () {
+    const verb = prompt('Enter german verb to conjugate:');
+    window.location.href = `https://conjugator.reverso.net/conjugation-german-verb-${verb}.html`;
+})();
