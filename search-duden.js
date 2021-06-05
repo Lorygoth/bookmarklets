840070c96853da1fcdd217f74023eab777f1ea3f
Yury <bad.wolf.gallifrey@gmail.com>
2021-06-06 00:45:16 +0200

    fix regex in example

--- search-duden.js
+++ search-duden.js
@@ -23,7 +23,7 @@ javascript:(function(){
     script.onload = scriptLoadedLogic;
     script.type="text/javascript";
     script.src = originalScriptUrl
-        .replace(/raw\.githubusercontent\.com\/(.*)\/\w+\/(\w+\.js)/, 'cdn.jsdelivr.net/gh/$1/$2');
+        .replace(/raw\.githubusercontent\.com\/(.*)\/[^\/]+\/([^\/]+\.js)$/, 'cdn.jsdelivr.net/gh/$1/$2');
     document.body.appendChild(script);
 })();
 */
