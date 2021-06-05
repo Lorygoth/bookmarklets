a6733ddf938419be84de3c6fb57d18c355122e8a
Yury <bad.wolf.gallifrey@gmail.com>
2021-06-06 00:17:14 +0200

    update duden search script

--- search-duden.js
+++ search-duden.js
@@ -1,4 +1,29 @@
-javascript: (function () {
-    const verb = prompt('Enter word to search by duden:');
-    window.location.href = `https://www.duden.de/rechtschreibung/${verb}`;
+window.lorygoth = {
+    ...(window.lorygoth || {}),
+    goduden: () => {
+        const verb = prompt('Enter word to search by duden:');
+        if (!verb) return;
+        window.location.href = `https://www.duden.de/rechtschreibung/${verb}`;
+    }
+};
+
+//example bookmarklet//
+
+/*
+javascript:(function(){
+    const originalScriptUrl = 'https://raw.githubusercontent.com/Lorygoth/bookmarklets/main/search-duden.js';
+    const scriptLoadedLogic = () => { window.lorygoth.goduden(); };
+
+    if (window.lorygoth && window.lorygoth.goduden) {
+        scriptLoadedLogic();
+        return;
+    }
+    
+    const script = document.createElement('script');
+    script.onload = scriptLoadedLogic;
+    script.type="text/javascript";
+    script.src = originalScriptUrl
+        .replace(/raw\.githubusercontent\.com\/(.*)\/\w+\/(\w+\.js)/, 'cdn.jsdelivr.net/gh/$1/$2');
+    document.body.appendChild(script);
 })();
+*/
